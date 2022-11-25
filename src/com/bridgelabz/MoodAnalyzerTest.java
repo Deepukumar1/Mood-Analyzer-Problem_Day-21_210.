94ecfc14b647d36cd2a8bf7650f558af6b62698f
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-25 20:23:18 +0530

    Added:  welcome to mood analayzer program

---
+++ src/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoodAnalyzerTest {
+
+	@BeforeAll
+    static void show_msg_before_all_test_case (){
+ System.out.println("---Welcome to Mood Analyzer---");
+}
+@AfterAll
+public static void show_Msg_After_All_Test_Case(){
+ System.out.println("All Test Case Completed");
+}
+@AfterEach
+     void after_Each_Test_Case_Print_Msg(){
+ System.out.println("Pass");
+}
+MoodAnalyser object ;
+@BeforeEach
+public void setUp() {
+ object = new MoodAnalyser();
+}
+@Test
+public void moodAnalyser(){
+ String ans = object.moodAnalyser("I am in Sad Mood");
+ Assertions.assertEquals("SAD",ans);;
+ System.out.println(ans);
+}
+}
